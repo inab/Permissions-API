722d2fb7bdbde711bd516e182e4def228d857682
acavalls <alejandro.canosa@bsc.es>
2021-06-09 14:49:04 +0000

    GET permissions endpoint (admin ~ DAC) -> Query validation calls + Error handling + Visas generation

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,44 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
+import { getFilePermissions, generateVisaPayload, signVisa } from '../utils/utils';
+import { validateQuery } from '../models/adminReadValidate';
+import getUsers from '../utils/getUsers';
+import createError from 'http-errors';
 
 export default ({ config, db, keycloak }) => {
 	let api = Router();
 
-	api.put('/user-requests', keycloak.protect('admin'), (req, res) => {
-		res.send("Hi there");
-		// Apply new permissions for an specific user and files on Permissions API.
-		// Apply new permissions for an specific user and files on Nextcloud Data Storage.
-	});
+	api.get('/', keycloak.protect(), async function(req, res){
+		// Check both x-account-id & account-id. At least one of them must exist.
+		let queryObject = {
+			headerId : req.header('x-account-id'),
+			paramsId : req.param('account-id')
+		}
+		// Validate with Joi.
+		const { error } = validateQuery(queryObject)
+
+		// No account ID present on Header || Parameters.
+		if(error) throw createError(400, "Bad request")
+
+		// Select userId for checking if exists on Keycloak.
+		let userId = req.header('x-account-id') ? req.header('x-account-id') : req.param('account-id')
+
+		// Getting the user list from Keycloak for the iPC realm.
+		const usersList = await getUsers()
+
+		// Filter user list by ID.
+		const isValidUser = usersList.find(user => user.id == userId);
+
+		// The current user does not exists on Keycloak.
+		if(!isValidUser){
+			throw createError(404, "User account invalid")
+		}
+
+		// Response: Get Visa array (JWT, PLAIN).
+		const allowedAccess = await getFilePermissions(isValidUser.id)	 
+		if(req.query.format === 'PLAIN') return res.send(generateVisaPayload(isValidUser.id, allowedAccess, 'PLAIN'));
+		res.send(await signVisa(generateVisaPayload(isValidUser.id, allowedAccess, 'JWT')));	
+	})
 
 	return api;
 }
