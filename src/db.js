442a634f310e0b125a0130cc7b22e60f6d1e0966
acavalls <alejandro.canosa@bsc.es>
2022-07-04 11:52:11 +0200

    MongoDB: Modifying configuration for working on different environments

--- src/db.js
+++ src/db.js
@@ -3,13 +3,14 @@ import winston from 'winston';
 require('dotenv').config();
 
 export default callback => {
-	let host = process.env.NODE_ENV === 'test' ? process.env.MONGO_HOST_TEST : process.env.MONGO_HOST;	
+	let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;	
 	let db = process.env.MONGO_DB_PERMISSIONS;
 	let username = process.env.MONGO_USER;
 	let password = process.env.MONGO_PASS;
 	let authSource = process.env.MONGO_AUTH;
-	// connect to MongoDB, then pass it to the callback fn:
-	const mongo = mongoose.connect(`mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`)
-		.then(() => winston.info(`Connected to ${db}`))
+        let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+            `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
+	const mongo = mongoose.connect(uri)
+            .then(() => winston.info(`Connected to ${db}`))
 	callback(mongo); 
 }
